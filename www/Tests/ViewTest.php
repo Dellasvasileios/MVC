3baaf9a31c117d8c6d41b7ef82e480e566a82c1f
Dellasvasileios <dellas.vasileios2@gmail.com>
2025-11-25 20:25:54 +0200

    first empty test

---
+++ www/Tests/ViewTest.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+final class ViewTest extends TestCase
+{
+    protected function setUp(): void
+    {
+
+    }
+
+    protected function tearDown(): void
+    {
+
+    }
+
+    private function removeDir(string $dir): void
+    {
+
+    }
+
+    public function testGetViewRendersTemplateWithData(): void
+    {
+
+    }
+
+    public function testGetViewThrowsWhenMissing(): void
+    {
+
+    }
+}
+
